32c507fcad4c886148c1eaac07f20dcd79aeb84c
Ruhatarhazhu <ikartik9@gmail.com>
2026-03-17 15:12:56 +0100

    Update products.js

--- js/products.js
+++ js/products.js
@@ -3,7 +3,7 @@ window.products = [
 name:"Sinsay M-es női pulóver",
 price:1290,
 images:["/images/product1.webp"],   // 🔥 EZ A LÉNYEG
-url:"/termek/sinsay-m-es-noi-pulover.html",
+url:"termek/sinsay-m-es-noi-pulover.html",
 vinted:"https://www.vinted.hu/items/8371202554-portland-noi-pulover",
 category:"Női ruházat",
 description:"Szép állapotú női pulóver"
