931b984a95d02724de0e6b8388e927c7ab2a3431
Ruhatarhazhu <ikartik9@gmail.com>
2026-03-17 13:21:58 +0100

    Update products.js

--- js/products.js
+++ js/products.js
@@ -4,14 +4,20 @@ const products = [
 name:"Sinsay női pulóver",
 price:1290,
 image:"/images/product1.webp",
-url:"/termek/sinsay-m-es-noi-pulover.html"
+url:"/termek/sinsay-m-es-noi-pulover.html",
+vinted:"https://www.vinted.hu/items/8371202554-portland-noi-pulover",
+category:"Női ruházat",
+description:"Szép állapotú női pulóver"
 },
 
 {
 name:"Laura Torelli női farmer nadrág",
 price:3990,
 image:"/images/laura.webp",
-url:"/termek/laura-torelli.html"
+url:"/termek/laura-farmer.html",
+vinted:"https://vinted.hu/...",
+category:"nadrág",
+description:"Kényelmes farmer nadrág"
 }
 
 ];
