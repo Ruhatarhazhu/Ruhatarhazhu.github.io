0a9fbc713a079df184256082389d5fd4c1f73050
Ruhatarhazhu <ikartik9@gmail.com>
2026-03-17 12:27:12 +0100

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,17 @@
+const products = [
+
+{
+name:"Janina leggings",
+price:1290,
+image:"/images/janina.webp",
+url:"/termek/janina-leggings.html"
+},
+
+{
+name:"Laura Torelli női farmer nadrág",
+price:3990,
+image:"/images/laura.webp",
+url:"/termek/laura-torelli.html"
+}
+
+];
